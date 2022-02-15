3546a822c4b2dd32cffea4c6c84263f913c5accc
karthihifi <karthi.hifi@gmail.com>
2022-02-15 20:51:20 +0530

    Initial Commit

---
+++ Shared/BookMarkManagement_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookMarkManagement_IOSApp.swift
+//  Shared
+//
+//  Created by Karthik on 15/02/22.
+//
+
+import SwiftUI
+
+@main
+struct BookMarkManagement_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
